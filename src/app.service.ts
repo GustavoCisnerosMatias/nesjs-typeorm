91ddf8467346a0c7ce6a560aa11db9467ab74c30
Gustavo C <skrillex58540808@gmail.com>
2024-02-29 21:02:12 -0500

    mi primer commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,29 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg';
 import config from './config';
-
 @Injectable()
 export class AppService {
   constructor(
     // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
+    @Inject('PG') private clientPG: Client,
   ) {}
   getHello(): string {
     const apiKey = this.configService.apiKey;
     const name = this.configService.database.name;
     return `Hello World! ${apiKey} ${name}`;
+  }
+
+  getTasks() {
+    return new Promise((resolve, reject) => {
+      this.clientPG.query('Select * from tasks', (err, res) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(res.rows);
+      });
+    });
   }
 }
